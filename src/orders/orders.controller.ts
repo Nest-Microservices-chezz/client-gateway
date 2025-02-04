b013df84fab6b297e447ffb05d685bfb22c6b22e
Grecia Delgado <greciadm2596@gmail.com>
2025-02-04 18:27:51 -0500

    Update pagination

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -6,10 +6,11 @@ import {
   Param,
   Inject,
   ParseUUIDPipe,
+  Query,
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ORDER_SERVICE } from 'src/config';
-import { CreateOrderDto } from './dto';
+import { CreateOrderDto, OrderPaginationDto } from './dto';
 import { firstValueFrom } from 'rxjs';
 
 @Controller('orders')
@@ -24,8 +25,9 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersClient.send('findAllOrders', {});
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    // return orderPaginationDto
+    return this.ordersClient.send('findAllOrders', orderPaginationDto);
   }
 
   @Get(':id')
