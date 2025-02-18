add341829be9eeebaf7f6e80eb5b9b9d59815597
Grecia Delgado <greciadm2596@gmail.com>
2025-02-18 10:44:39 -0500

    update custom error

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -27,9 +27,16 @@ export class OrdersController {
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    // return orderPaginationDto
-    return this.transportClient.send('findAllOrders', orderPaginationDto);
+  async findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    try {
+      const orders = await firstValueFrom(
+        this.transportClient.send('findAllOrders', orderPaginationDto),
+      );
+
+      return orders;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get('id/:id')
