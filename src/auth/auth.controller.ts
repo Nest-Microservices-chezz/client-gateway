bd3128f9b7eafb2a2298512ad93083514973abcf
Grecia Delgado <greciadm2596@gmail.com>
2025-03-12 17:35:53 -0500

    Add login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
+import { error } from 'console';
 
 @Controller('auth')
 export class AuthController {
@@ -11,12 +13,22 @@ export class AuthController {
 
   @Post('register')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
-    return this.transportClient.send('auth.register.user', registerUserDto);
+    return this.transportClient
+      .send('auth.register.user', registerUserDto)
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
   }
 
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
-    return this.transportClient.send('auth.login.user', loginUserDto);
+    return this.transportClient.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
   }
 
   @Get('verify')
