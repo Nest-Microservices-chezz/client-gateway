76fa9876402bff08dbf343559a65996e46290f21
Grecia Delgado <greciadm2596@gmail.com>
2025-03-12 15:24:18 -0500

    auth module configure

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(){}
+  constructor(
+    @Inject(NATS_SERVICE) private readonly transportClient: ClientProxy,
+  ) {}
 
   @Post('register')
-  registerUser(){}
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.transportClient.send('auth.register.user', {});
+  }
 
   @Post('login')
-  loginUser(){}
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.transportClient.send('auth.login.user', {});
+  }
 
-  @Post('verify')
-  verifyUser(){}
+  @Get('verify')
+  verifyToken() {
+    return this.transportClient.send('auth.verify.user', {});
+  }
 }
